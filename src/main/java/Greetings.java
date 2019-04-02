ab06496562cd5b4923eaaa72c7003ad1e626bb5c
Mzwa <m.shawnlouw@gmail.com>
2019-04-02 15:51:12 +0200

    1st commit

---
+++ src/main/java/Greetings.java
@@ -0,0 +1,59 @@
+import java.util.HashMap;
+
+public class Greetings {
+//    I need a var list to store names
+//    the username should add a string of a selected language type
+//    I need a counter for each person greeted - so I need a method n if statement that will increment if same name == same name
+
+    private HashMap<String, Integer> users = new HashMap<String, Integer>();
+    // WILL GET THE SIZE OF ALL USERS
+   //WILL COUNT FOR SAME USER ENTERED AND INCREMENT BY ONE IF REPEATED
+     //EACH PERSON --- I'LL PUSH HIM TO USERS
+
+    public Greetings(){
+//        user = username;
+//        count = personCount;
+    }
+// ----I WANT TO ADD EACH PERSON ON THE LIST AND GET THE SIZE OF PEOPLE IN THE LIST
+//***** I NEED A ENUM(extended) THAT WILL HAVE TYPES OF LANGUAGES AND A CLASS OVERRIDING THOSE LANGUAGES BY ADDING ****
+//******************* BY ADDING ANY NAME + SELECTED LANGUAGE *********************
+
+    public HashMap<String, Integer> addUsers(String name, int counter){
+        int counterForEachPerson = counter;
+        if(users.size() == 0 ){
+            users.put(name, counterForEachPerson);
+            if(users.containsKey(users==users)){
+                counterForEachPerson++;
+            }
+        }
+        return users;
+    }
+
+//--**-- HERE I WANT TO RETURN ALL THE USERS THAT ARE ADDED --**--//
+    public static void main(String [] args){
+        Greetings greet = new Greetings();
+        Greetings greet2 = new Greetings();
+        Greetings greet3 = new Greetings();
+
+        System.out.println(greet.addUsers("Mzwa", 4));
+        System.out.println(greet2.addUsers("Sipho", 22));
+        System.out.println(greet3.addUsers("Sakhi", 1));
+        System.out.println(greet.addUsers("Sakhi", 1));
+
+//        System.out.println(greet.users.keySet());
+
+    }
+
+//----I WANT TO CREATE A METHOD THAT WILL RETURN NAMES +++ LANGUAGES --------
+    public String greets(String username, String language){
+        if(language == "IsiXhosa"){
+            String greetPerson = "Molo " + username;
+            return greetPerson;
+        }
+//        else if (language == "English"){
+//            String greetPerson = "Hello %s", username;
+//        }
+    }
+//-----------------------   TEST   USING   MAIN   --------------------
+// -----------------------
+}
